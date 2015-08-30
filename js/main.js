bf7d541e39cd4036dd68425c4e89ce2d2ecf08bb
Vignesh Ramesh <r.vigs23@gmail.com>
2015-08-29 22:48:38 -0700

    Added functionality to store items (with images) in parse and retrieve them on fb return log in.

--- js/main.js
+++ js/main.js
@@ -30,6 +30,10 @@ function addItemToList() {
     
     console.log("Adding item to list");
     
+    /*-----
+    Adding item to display 
+    -----*/
+    
     newItem = '<div class="three columns">' +
             '<div class="u-full-width" style="height:120px; background-size: auto 120px; background-repeat: no-repeat; background-position: center; background-color: #ECECEC; background-image:url(\''+itemImgURL+'\')"/>' +
             '<h6>'+$('#itemDesc').val()+'</h6></div>';
@@ -50,5 +54,79 @@ function addItemToList() {
     }
     
     $('.add-item').addClass("hidden");
+    
+    
+    /*-----
+    Saving item to parse file storage
+    ------*/
+    
+    var fileUploadControl = $("#itemPhoto")[0];
+    if (fileUploadControl.files.length > 0) {
+      var file = fileUploadControl.files[0];
+      var name = "photo.jpg";
+      var parseFile = new Parse.File(name, file);
+    }
+    parseFile.save().then(function() {
+      console.log("item photo saved");
+      
+      //adding Parse object to Items Database
+      var item = new Parse.Object("Item");
+      item.set("user", Parse.User.current());
+      item.set("itemDesc", $("#itemDesc").val());
+      item.set("itemPhoto", parseFile);
+      item.set("itemPrice", $("#itemPrice").val());
+      item.save();
+        
+    }, function(error) {
+      console.log("error saving item photo: " + error);
+    });
+    
 
+}
+
+function checkForUserItems() {
+    
+    console.log("Checking for previous items...");
+    
+    var Item = Parse.Object.extend("Item");
+    var query = new Parse.Query(Item);
+    query.equalTo("user", Parse.User.current());
+    query.find({
+      success: function(results) {
+        console.log("Successfully retrieved " + results.length + " items.");
+        // Do something with the returned Parse.Object values
+        for (var i = 0; i < results.length; i++) {
+            var object = results[i];
+            console.log("Adding " + object.get("itemDesc") + " to the list");
+            
+            var itemDesc = object.get("itemDesc");
+            var itemImgURL = object.get("itemPhoto").url();
+            
+            newItem = '<div class="three columns">' +
+            '<div class="u-full-width" style="height:120px; background-size: auto 120px; background-repeat: no-repeat; background-position: center; background-color: #ECECEC; background-image:url(\''+itemImgURL+'\')"/>' +
+            '<h6>'+itemDesc+'</h6></div>';
+    
+            if(itemsAdded < maxItemsPerRow) {
+                $('.row-'+rowsAdded+'').append(newItem);
+                itemsAdded++;
+            }
+
+            else {
+                rowsAdded++;
+                itemsAdded=0;
+                newRowItem = '<div class="row row-'+rowsAdded+'"> ' +
+                            newItem +
+                            '</div';
+
+                $('.list-items-added').append(newRowItem);
+            } 
+        }
+      },
+      error: function(error) {
+        console.log("Error: " + error.code + " " + error.message);
+      }
+    });
+    
+    
+    
 }
